735f2d0529bed7a28dd7a2b7cb53f8ec3e45d29b
gabrieuu <81256082+gabrieuu@users.noreply.github.com>
2021-12-14 09:58:55 -0300

    consertando erro botao

--- TCC/src/main/java/tcc/repositorio/AlternativaRepository.java
+++ TCC/src/main/java/tcc/repositorio/AlternativaRepository.java
@@ -8,6 +8,11 @@ import tcc.dominio.Alternativa;
 
 public interface AlternativaRepository extends JpaRepository<Alternativa, Integer> {
 	
-    @Query("select a from Alternativa a where a.id_componente like %:idComp%"+ "and a.id_defeito like %:idDef%") Alternativa findByIds(@Param("idComp") int idComp, @Param("idDef") int idDef);
+    @Query("select a from Alternativa a "
+    		+ "JOIN a.componente c "
+    		+ "JOIN a.defeito d "
+    		+ "where c.idComponente = :idComp "
+    		+ "and d.idDefeito = :idDef") 
+     Alternativa findByIds(@Param("idComp") int idComp, @Param("idDef") int idDef);
 
 }
